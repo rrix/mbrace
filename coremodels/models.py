b15394f0c9e465a4610947d7ecdc2db734773aae
Ryan Rix <ry@n.rix.si>
2013-05-29 05:07:21 -0700

    Use django auth classes for User
    We create a Hugger and OneToOne it with the Django User type as in https://docs.djangoproject.com/en/dev/topics/auth/customizing/#extending-
    the-existing-user-model

--- coremodels/models.py
+++ coremodels/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):
+
+class Hugger(models.Model):
+    user = models.OneToOneField(User)
     name = models.CharField(max_length=20)
     zip_code = models.CharField(max_length=5)
     last_location = models.CharField(max_length=20) # TODO: Pull GeoDjango in to the mix
 
 class Meeting(models.Model):
-    user_in_need = models.ForeignKey('User', related_name='requestor_set') 
-    user_delivering = models.ForeignKey('User', related_name='deliverer_set')
+    user_in_need = models.ForeignKey('Hugger', related_name='requestor_set') 
+    user_delivering = models.ForeignKey('Hugger', related_name='deliverer_set')
     review_1 = models.ForeignKey('Review', related_name='requestor_review')
     review_2 = models.ForeignKey('Review', related_name='deliverer_review')
 
@@ -15,6 +18,6 @@ class Review(models.Model):
     rating = models.SmallIntegerField()
     notes = models.TextField()
 
-    author = models.ForeignKey('User', related_name='review_author')
-    owner = models.ForeignKey('User', related_name='review_for')
+    author = models.ForeignKey('Hugger', related_name='review_author')
+    owner = models.ForeignKey('Hugger', related_name='review_for')
 
