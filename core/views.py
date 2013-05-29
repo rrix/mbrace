68b8bc157d97a5f76392b254f296c067c1b11597
Ryan Rix <ry@n.rix.si>
2013-05-29 06:19:08 -0700

    Show basic login and register and logout pages

--- core/views.py
+++ core/views.py
@@ -1,7 +1,24 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login
 
 
 @login_required
 def index(request):
     return render(request, 'core/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return HttpResponseRedirect("/")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {
+        'form': form,
+    })
--- mbrace/urls.py
+++ mbrace/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     url(r'^$', include('core.urls')),
 
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/register/$', 'core.views.register'),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
