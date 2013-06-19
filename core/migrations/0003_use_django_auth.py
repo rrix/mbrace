209a82867422a2f83075777fc8abccfc8e45684c
Ryan Rix <ry@n.rix.si>
2013-06-19 00:18:25 -0700

    Fix migrations to match reality

--- core/migrations/0003_use_django_auth.py
+++ core/migrations/0003_use_django_auth.py
@@ -9,56 +9,56 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Deleting model 'User'
-        db.delete_table(u'coremodels_user')
+        db.delete_table(u'core_user')
 
         # Adding model 'Hugger'
-        db.create_table(u'coremodels_hugger', (
+        db.create_table(u'core_hugger', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=20)),
             ('zip_code', self.gf('django.db.models.fields.CharField')(max_length=5)),
             ('last_location', self.gf('django.db.models.fields.CharField')(max_length=20)),
         ))
-        db.send_create_signal(u'coremodels', ['Hugger'])
+        db.send_create_signal(u'core', ['Hugger'])
 
 
         # Changing field 'Meeting.user_in_need'
-        db.alter_column(u'coremodels_meeting', 'user_in_need_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['coremodels.Hugger']))
+        db.alter_column(u'core_meeting', 'user_in_need_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Hugger']))
 
         # Changing field 'Meeting.user_delivering'
-        db.alter_column(u'coremodels_meeting', 'user_delivering_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['coremodels.Hugger']))
+        db.alter_column(u'core_meeting', 'user_delivering_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Hugger']))
 
         # Changing field 'Review.owner'
-        db.alter_column(u'coremodels_review', 'owner_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['coremodels.Hugger']))
+        db.alter_column(u'core_review', 'owner_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Hugger']))
 
         # Changing field 'Review.author'
-        db.alter_column(u'coremodels_review', 'author_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['coremodels.Hugger']))
+        db.alter_column(u'core_review', 'author_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Hugger']))
 
     def backwards(self, orm):
         # Adding model 'User'
-        db.create_table(u'coremodels_user', (
+        db.create_table(u'core_user', (
             ('zip_code', self.gf('django.db.models.fields.CharField')(max_length=5)),
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=20)),
             ('last_location', self.gf('django.db.models.fields.CharField')(max_length=20)),
         ))
-        db.send_create_signal(u'coremodels', ['User'])
+        db.send_create_signal(u'core', ['User'])
 
         # Deleting model 'Hugger'
-        db.delete_table(u'coremodels_hugger')
+        db.delete_table(u'core_hugger')
 
 
         # Changing field 'Meeting.user_in_need'
-        db.alter_column(u'coremodels_meeting', 'user_in_need_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['coremodels.User']))
+        db.alter_column(u'core_meeting', 'user_in_need_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.User']))
 
         # Changing field 'Meeting.user_delivering'
-        db.alter_column(u'coremodels_meeting', 'user_delivering_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['coremodels.User']))
+        db.alter_column(u'core_meeting', 'user_delivering_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.User']))
 
         # Changing field 'Review.owner'
-        db.alter_column(u'coremodels_review', 'owner_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['coremodels.User']))
+        db.alter_column(u'core_review', 'owner_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.User']))
 
         # Changing field 'Review.author'
-        db.alter_column(u'coremodels_review', 'author_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['coremodels.User']))
+        db.alter_column(u'core_review', 'author_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.User']))
 
     models = {
         u'auth.group': {
@@ -97,7 +97,7 @@ class Migration(SchemaMigration):
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        u'coremodels.hugger': {
+        u'core.hugger': {
             'Meta': {'object_name': 'Hugger'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_location': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
@@ -105,22 +105,22 @@ class Migration(SchemaMigration):
             'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'}),
             'zip_code': ('django.db.models.fields.CharField', [], {'max_length': '5'})
         },
-        u'coremodels.meeting': {
+        u'core.meeting': {
             'Meta': {'object_name': 'Meeting'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'review_1': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'requestor_review'", 'to': u"orm['coremodels.Review']"}),
-            'review_2': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'deliverer_review'", 'to': u"orm['coremodels.Review']"}),
-            'user_delivering': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'deliverer_set'", 'to': u"orm['coremodels.Hugger']"}),
-            'user_in_need': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'requestor_set'", 'to': u"orm['coremodels.Hugger']"})
+            'review_1': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'requestor_review'", 'to': u"orm['core.Review']"}),
+            'review_2': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'deliverer_review'", 'to': u"orm['core.Review']"}),
+            'user_delivering': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'deliverer_set'", 'to': u"orm['core.Hugger']"}),
+            'user_in_need': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'requestor_set'", 'to': u"orm['core.Hugger']"})
         },
-        u'coremodels.review': {
+        u'core.review': {
             'Meta': {'object_name': 'Review'},
-            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'review_author'", 'to': u"orm['coremodels.Hugger']"}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'review_author'", 'to': u"orm['core.Hugger']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'notes': ('django.db.models.fields.TextField', [], {}),
-            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'review_for'", 'to': u"orm['coremodels.Hugger']"}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'review_for'", 'to': u"orm['core.Hugger']"}),
             'rating': ('django.db.models.fields.SmallIntegerField', [], {})
         }
     }
 
-    complete_apps = ['coremodels']
+    complete_apps = ['core']
