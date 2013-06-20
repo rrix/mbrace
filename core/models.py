151e2fc97986ed0b849945d414a8362f5863883b
Ryan Rix <ry@n.rix.si>
2013-06-19 20:06:58 -0700

    add phone_number to hugger profile model

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ class Hugger(models.Model):
     name = models.CharField(max_length=20)
     zip_code = models.CharField(max_length=5)
     last_location = models.CharField(max_length=20) # TODO: Pull GeoDjango in to the mix
+    phone_number = models.CharField(max_length=20) # XXX: Make sure this is US style using https://docs.djangoproject.com/en/1.4/ref/contrib/localflavor/#django.contrib.localflavor.us.models.PhoneNumberField
 
 class Meeting(models.Model):
     user_in_need = models.ForeignKey('Hugger', related_name='requestor_set') 
