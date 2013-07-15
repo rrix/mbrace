f37ce3776c5021d997d5f05535e564564fd2ee4d
Ryan Rix <ry@n.rix.si>
2013-07-15 15:53:21 -0700

    Switch from django-scss to django-pipeline

--- mbrace/settings.py
+++ mbrace/settings.py
@@ -44,39 +44,8 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -209,3 +178,52 @@ SOUTH_MIGRATION_MODULES = {
     'django_facebook': 'ignore',
 }
 
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'public')
+STATIC_URL = '/public/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+PIPELINE_ROOT=os.path.join(STATIC_ROOT, 'pipeline')
+PIPELINE_CSS = {
+    'foundation': {
+        'source_filenames': {
+            'foundation/scss/normalize.scss',
+            'foundation/scss/foundation.scss',
+            'foundation/scss/foundation/_variables.scss',
+            'foundation/scss/foundation/components/*.scss'
+        },
+        'output_filename': 'foundation.css'
+    },
+    'app': {
+        'source_filenames': {
+            'scss/app.scss'
+        },
+        'output_filename': 'app.css'
+    }
+}
+
+PIPELINE_JS = {
+    'foundation': {
+        'source_filenames': {
+            'foundation/js/vendor/jquery.js',
+            'foundation/js/vendor/custom.modernizr.js',
+            'foundation/js/index.js'
+            'foundation/js/foundation/foundation*.js'
+        },
+        'output_filename': 'foundation.js'
+    }
+}
+
+PIPELINE_ENABLED=True
+PIPELINE_CSS_COMPRESSOR=None
+PIPELINE_JS_COMPRESSOR=None
+PIPELINE_COMPILERS = (
+    'pipeline_compass.compiler.CompassCompiler',
+)
