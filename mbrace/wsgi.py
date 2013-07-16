f53ad9c6860378ffdc283e1342bcb4fc52756137
Ryan Rix <ry@n.rix.si>
2013-07-15 19:55:18 -0700

    Start using dj_static which appears to make staticfiles simple as fuck

--- mbrace/wsgi.py
+++ mbrace/wsgi.py
@@ -5,7 +5,8 @@ sys.path.append('/home/rrix/dev/mbrace')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mbrace.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 import djcelery
 djcelery.setup_loader()
